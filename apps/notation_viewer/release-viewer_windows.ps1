cbf05cf0538258eb6a512b1d764e76f51ac26bd9
YJ Park <yjpark@gmail.com>
2021-12-13 12:53:24 +0800

    add some script to release binary

---
+++ apps/notation_viewer/release-viewer_windows.ps1
@@ -0,0 +1,7 @@
+cargo build --release --features native
+
+cd ..\..\
+bash -c "rm -rf release/windows/notation_viewer"
+bash -c "mkdir -p release/windows/notation_viewer"
+bash -c "cp -v target/release/notation_viewer.exe release/windows/notation_viewer/"
+bash -c "cp -vr apps/notation_viewer/assets release/windows/notation_viewer/"
---
+++ crates/publish-crates.ps1
@@ -0,0 +1,27 @@
+cd notation_core
+cargo publish
+
+cd ../notation_fretted
+cargo publish
+
+cd ../notation_guitar
+cargo publish
+
+cd ../notation_proto
+cargo publish
+
+cd ../notation_model
+cargo publish
+
+cd ../notation_dsl
+cargo publish
+
+cd ../notation_midi
+cargo publish
+
+cd ../notation_bevy_utils
+cargo publish
+
+cd ../notation_bevy
+cargo publish
+
