c72a202a5dc3e277f6a6f8baf87a953e1f2871a2
YJ Park <yjpark@gmail.com>
2021-12-16 20:48:30 +0800

    update script

--- apps/notation_viewer/release-viewer_windows.ps1
+++ apps/notation_viewer/release-viewer_windows.ps1
@@ -5,4 +5,6 @@ bash -c "rm -rf release/windows/notation_viewer"
 bash -c "mkdir -p release/windows/notation_viewer"
 bash -c "cp -v target/release/notation_viewer.exe release/windows/notation_viewer/"
 bash -c "cp -vr apps/notation_viewer/assets release/windows/notation_viewer/"
+bash -c "rm -rf ~/Applications/notation_viewer"
+bash -c "cp -vr release/windows/notation_viewer/ ~/Applications/"
 cd apps\notation_viewer
